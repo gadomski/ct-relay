5cba2d6c599e68a6db647f58d6ad5bb0c01e9b40
Pete Gadomski <pete.gadomski@gmail.com>
2025-07-13 10:30:34 -0600

    feat: add some text

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -1,14 +1,33 @@
-import { Heading, Span, Stack, Text } from "@chakra-ui/react";
+import {
+  Blockquote,
+  Heading,
+  Highlight,
+  Span,
+  Stack,
+  Text,
+} from "@chakra-ui/react";
+
+const KELLY = {
+  fontWeight: "bold",
+  bg: "blue.emphasized",
+  color: "blue.fg",
+};
+
+const BEX = {
+  fontWeight: "bold",
+  bg: "green.emphasized",
+  color: "green.fg",
+};
 
 export default function Sidebar() {
   return (
     <Stack px={4} pt={6} gap={4}>
       <Heading>
-        <Span fontWeight="bold" bg="green.emphasized" color="green.fg" p="1">
+        <Span p="1" {...BEX}>
           Bex 🚴‍♀️
         </Span>{" "}
         and{" "}
-        <Span fontWeight={"bold"} bg="blue.emphasized" color="blue.fg" p="1">
+        <Span p="1" {...KELLY}>
           Kelly 🏃‍♀️
         </Span>{" "}
         relay the <abbr title="Colorado Trail">CT</abbr>
@@ -16,6 +35,24 @@ export default function Sidebar() {
       <Text fontWeight={"lighter"} fontSize={"sm"}>
         Summer 2025
       </Text>
+      <Blockquote.Root>
+        <Blockquote.Content>
+          <Highlight query={"Kelly"} styles={KELLY}>
+            The Colorado trail is 490 miles (From Denver to Durango) with
+            89,000' of elevation gain. The route also traverses six wilderness
+            areas. Just before and after each of these wilderness areas is where
+            Kelly and I will exchange our baton and she will run through those
+            sections, as those zones do not allow any wheels on the ground! The
+            wilderness sections are especially beautiful and rugged!
+          </Highlight>
+        </Blockquote.Content>
+        <Blockquote.Caption>
+          —{" "}
+          <cite>
+            <Span {...BEX}>Bex</Span>
+          </cite>
+        </Blockquote.Caption>
+      </Blockquote.Root>
     </Stack>
   );
 }
