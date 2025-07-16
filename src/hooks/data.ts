f64dec199b6ec8ed138d7eda3bf7f83dc8d76230
Pete Gadomski <pete.gadomski@gmail.com>
2025-07-16 10:30:00 -0600

    fix: use public folder

--- src/hooks/data.ts
+++ src/hooks/data.ts
@@ -144,7 +144,7 @@ function useDataFetch<T>(path: string) {
   const { data } = useQuery<T>({
     queryKey: [path],
     queryFn: async () => {
-      return await fetch(new URL("../data/" + path, import.meta.url).href).then(
+      return await fetch(import.meta.env.BASE_URL + "/" + path).then(
         (response) => {
           if (response.ok) {
             return response.json();
